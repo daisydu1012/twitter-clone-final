eb18c9e539adca91eced09e2e46adfffe66feb3b
daisydu1012 <daisydu1012@gmail.com>
2026-05-11 17:21:34 -0700

    Finalize twitter clone project

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,23 +1,7 @@
-from flask.cli import FlaskGroup
+from project import create_app, db
 
-from project import app, db, User
-
-
-cli = FlaskGroup(app)
-
-
-@cli.command("create_db")
-def create_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
-
-@cli.command("seed_db")
-def seed_db():
-    db.session.add(User(email="michael@herman.org"))
-    db.session.commit()
+app = create_app()
 
 
 if __name__ == "__main__":
-    cli()
+    app.run(host="0.0.0.0")
